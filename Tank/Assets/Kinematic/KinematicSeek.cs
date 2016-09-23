b779cd2055a02a11b8895e847643c7ab176db26c
Pau5erra <pausc1@TR12.CAT>
2016-09-23 09:59:07 +0200

    tank

---
+++ Tank/Assets/Kinematic/KinematicSeek.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicSeek : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (move.mov_velocity.magnitude > 0)
+        {
+            Vector3 move1 = new Vector3(1.0f, 0.0f, 1.0f);
+            // TODO 5: Set movement velocity to max speed in the direction of the target
+          //  move.mov_velocity = move1 * move.max_mov_velocity;
+        }
+    }
+}
